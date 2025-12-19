c246a7f6b32ad8cc5f08f5f582e7b7774e1d2c99
Tourtelier Eloan <eloantourtelier@gmail.com>
2025-12-19 15:10:39 +0100

    [ADD] added resume (without personnal info)

--- src/home/HomeView.vue
+++ src/home/HomeView.vue
@@ -167,6 +167,7 @@
         <a href="https://github.com/El-oan" target="_blank" class="link-button"> GitHub </a>
         <a href="https://x.com/eeloannn" target="_blank" class="link-button"> Twitter </a>
         <a href="https://www.instagram.com/eloan.trtl/" target="_blank" class="link-button"> Instagram </a>
+        <a href="/resume.pdf" class="link-button" download> Resume (PDF) </a>
         <a href="https://apps.apple.com/us/app/%E6%96%87-character/id6747664971" target="_blank" class="link-button">
           Download my App
         </a>
